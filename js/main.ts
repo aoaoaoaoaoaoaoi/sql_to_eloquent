b1e720c745694fb78c39af07684ae2dcb946d924
unknown <gummi.bear456aaaaa@gmail.com>
2020-11-13 03:10:07 +0900

    文字列の分解

--- js/main.ts
+++ js/main.ts
@@ -1,4 +1,19 @@
 function conversion(value: string) : void
 {
-  
+  let nest = 0;
+  let beforeChar = '';
+  let sql: string[] = new Array();
+  for(var i = 0; i < value.length; ++i){
+    let current = value[i];
+
+    sql[nest] += current;
+    if(value[i] == '('){
+      ++nest;
+    }
+    else if(value[i] == ')'){
+      --nest;
+      //名前があるから微妙
+    }
+    beforeChar = current;
+  }
 }
