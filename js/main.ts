a8568eae94de6f65a767793733a1dc632f5e4e85
unknown <gummi.bear456aaaaa@gmail.com>
2020-11-16 23:49:42 +0900

    1116

--- js/main.ts
+++ js/main.ts
@@ -7,12 +7,16 @@ function conversion(value: string) : void
 {
   let nest = 0;
   let beforeChar = '';
+  let selectNest = [];
   let sql: string[] = new Array();
   for(var i = 0; i < value.length; ++i){
     let current = value[i];
 
     sql[nest] += current;
     if(isSelect(value.substr(i))){
+      
+    }
+    else if(value[i] == '('){
       ++nest;
     }
     else if(value[i] == ')'){
