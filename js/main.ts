bd2270d637b33536542c36d4b732a0b3970c087c
unknown <gummi.bear456aaaaa@gmail.com>
2020-11-17 00:20:47 +0900

    ネストを保存できるように修正

--- js/main.ts
+++ js/main.ts
@@ -8,20 +8,21 @@ function conversion(value: string) : void
   let nest = 0;
   let beforeChar = '';
   let selectNest = [];
+  let selectCount = 0;
   let sql: string[] = new Array();
   for(var i = 0; i < value.length; ++i){
     let current = value[i];
 
     sql[nest] += current;
     if(isSelect(value.substr(i))){
-      
+      selectNest[selectCount] = nest;
+      ++selectCount;
     }
     else if(value[i] == '('){
       ++nest;
     }
     else if(value[i] == ')'){
       --nest;
-      //名前があるから微妙
     }
     beforeChar = current;
   }
