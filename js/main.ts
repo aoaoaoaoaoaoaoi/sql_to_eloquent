0cc31adf64b4abd1d343d060bd6909787f2560c2
unknown <gummi.bear456aaaaa@gmail.com>
2020-11-24 23:32:25 +0900

    1124

--- js/main.ts
+++ js/main.ts
@@ -3,6 +3,11 @@ function isSelect(value: string) : boolean
   return value.substr(0, 7) === "select ";
 }
 
+function isFrom(value: string) : boolean
+{
+  return value.substr(0, 5) === "from ";
+}
+
 function conversion(value: string) : void
 {
   let nest = 0;
@@ -11,14 +16,34 @@ function conversion(value: string) : void
   let selectCount = 0;
   let sql: string[] = new Array();
   let queryBuilder: string[] = new Array();
+  let queryBuilderStr = "";
   for(var i = 0; i < value.length; ++i){
     let current = value[i];
     let isAlias = false;
 
     if(isSelect(value.substr(i))){
       i += 7;
-      for(var j = i; j < value.length; ++j){
-        queryBuilder
+      for(; i < value.length; ++i){
+        if(isFrom(value.substr(i))){
+          i += 5;
+          let from = "";
+          let isBig = true;
+          for(; i < value.length; ++i){
+            if(value[i] == ''){
+              continue;
+            }else if(value[i] == '_'){
+              isBig = true;
+            }
+            else if(isBig){
+              from += value[i].toUpperCase();
+              isBig = false;
+            }else{
+              from += value[i];
+            }
+          }
+          queryBuilderStr += from;
+        }
+        queryBuilderStr += "->get();"
       }
       
 
